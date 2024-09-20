6708c8f212d8ffd8bf6f123d4e29dff20bf39a08
leduykhang2k <leduykhang232@gmail.com>
2024-09-20 17:04:06 +0700

    update

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SignupScreen} from './screens';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SignupScreen />
+    </SafeAreaProvider>
   );
-}
+};
+export default App
 
 const styles = StyleSheet.create({
   container: {
---
+++ screens/index.js
@@ -0,0 +1,6 @@
+import { LoginScreen } from './LoginScreen';
+import { SignupScreen } from './SignupScreen';
+import { ForgotPasswordScreen } from './ForgotPasswordScreen.js';
+import { HomeScreen } from './HomeScreen.js';
+
+export {LoginScreen, SignupScreen, ForgotPasswordScreen, HomeScreen};
